6e89ad58494f07be127c83fdf2447b0b4f75c073
Max Gardenswartz <max.gardenswartz@proton.me>
2026-03-17 18:16:24 -0400

    v1 ekr

--- scripts/ekf.py
+++ scripts/ekf.py
@@ -4,73 +4,49 @@ import equinox as eqx
 from typing import Callable
 from jaxtyping import Array, Float
 import diffrax
-from sympy import numer
 
 class EKF(eqx.Module):
     f_sys: Callable  # Continuous-time dynamical model
     h_sys: Callable  # Continuous-time measurement model
-    Q: Float[Array, "nz nz"]  # Measurement noise (PD? PSD? symmetric?)
-    R: Float[Array, "nx nx"]  # Process noise (PD? PSD? symmetric?)
+    Q: Float[Array, "nz nz"]  # Measurement noise (PD symmetric)
+    R: Float[Array, "nx nx"]  # Process noise (PD symmetric)
     
-    def step(
+    def propagate(
         self,
         mu_prev: Float[Array, "nx"], 
         Sigma_prev: Float[Array, "nx nx"],
-        u: Float[Array, "nu"], 
-        z: Float[Array, "nz"],
-        t_prev: float,
+        u: Float[Array, "nu"], # control input
+        z: Float[Array, "nz"], # measurement
         dt: float
     ) -> tuple[Float[Array, "nx"], Float[Array, "nx nx"]]:
-        mu_overbar, Sigma_overbar = self.predict(
-            mu_prev=mu_prev,
-            Sigma_prev=Sigma_prev,
-            t_prev=t_prev,
-            u=u,
-            dt=dt
-        )
-        return mu_overbar, Sigma_overbar
-        # x_new, Sigma_new = self.update(x_pred, Sigma_pred, z)
-        # return x_new, Sigma_new
-
-
-    # def update(self, x_pred, Sigma_pred, z):
-    #     # H is the Jacobian of h_sys with respect to the first argument (x)
-    #     # evaluated at x_pred.
-    #     H = jax.jacfwd(self.h_sys)(x_pred)
-
-    def predict(
-        self,
-        mu_prev: Float[Array, "nx"],
-        Sigma_prev: Float[Array, "nx nx"],
-        u: Float[Array, "nu"],
-        t_prev: float,
-        dt: float
-    ) -> tuple[Float[Array, "nx"], Float[Array, "nx nx"]]:
-        
-        # propagate mean
+        # predict: propagate mean
         def vector_field(t, x, args): return self.f_sys(x, u)
         ode_term = diffrax.ODETerm(vector_field)
         solver = diffrax.Dopri5() # Runge-Kutta 4/5
         sol = diffrax.diffeqsolve(
             ode_term,
             solver, 
-            t0=t_prev, 
-            t1=(t_prev + dt), # Is this right? It's not t_prev + 10 * dt or anything right?
+            t0=0.0, 
+            t1=dt,
             dt0=dt, 
             y0=mu_prev
         )
         if sol.result != diffrax.RESULTS.successful:
             raise RuntimeError(f"SIMULATION FAILED: Diffrax Error {sol.result}")
-        mu_overbar_now = sol.ys[-1] # Is the [-1] needed? Can the "None" typecast be fixed here?
+        mu_overbar_now = sol.ys[-1] # Can the "None" typecast be fixed here? It's still appearing.s
         
-        # propagate covariance
+        # predict: propagate covariance
         df_dx = jax.jacfwd(self.f_sys)(mu_prev, u)
-        Sigma_overbar_now = self.R + jnp.matmul( # Does this support jit? Is this the right way to write a three-matrix prod?
-            jnp.matmul(df_dx, Sigma_prev),
-            df_dx.T
-        )
-
-        return mu_overbar_now, Sigma_overbar_now
+        Sigma_overbar_now = self.R + df_dx @ Sigma_prev @ df_dx.T
+    
+        # measurement update
+        dh_dx = jax.jacfwd(self.h_sys)(mu_overbar_now) # Note: not mu_prev
+        temp_prod = Sigma_overbar_now @ dh_dx.T
+        K_kalman = temp_prod @ jnp.linalg.inv(dh_dx @ temp_prod + self.Q)
+        mu_now = mu_overbar_now + K_kalman @ (z - self.h_sys(mu_overbar_now))
+        nx = Sigma_overbar_now.shape[0]
+        Sigma_now = (jnp.eye(nx) - K_kalman @ dh_dx) @ Sigma_overbar_now
+        return mu_now, Sigma_now
 
 def f(x_prev: Float[Array, "nx"], u: Float[Array, "nu"] ) -> Float[Array, "nx"]:
     x_1 = x_prev[0]
@@ -79,15 +55,15 @@ def f(x_prev: Float[Array, "nx"], u: Float[Array, "nu"] ) -> Float[Array, "nx"]:
     u_1 = u[0]
     u_2 = u[1]
     u_3 = u[2]
-    x_now = jnp.array([
-        [x_1 + u_2 * jnp.cos(x_3 + u_1)], 
-        [x_2 + u_2 * jnp.sin(x_3 + u_1)],
-        [x_3 + u_3 + u_1]
+    x_dot = jnp.array([
+        x_1 + u_2 * jnp.cos(x_3 + u_1), 
+        x_2 + u_2 * jnp.sin(x_3 + u_1),
+        x_3 + u_3 + u_1
     ], dtype=jnp.float32
     )
-    return x_now
+    return x_dot.T
 
-def h(x_prev: Float[Array, "nx"]) -> Float[Array, "ny"]:
+def h(x_prev: Float[Array, "nx"]) -> Float[Array, "nz"]:
     L_x = 0
     L_y = 0
     x_1 = x_prev[0]
@@ -103,13 +79,33 @@ def h(x_prev: Float[Array, "nx"]) -> Float[Array, "ny"]:
     return jnp.array([h_x], dtype=jnp.float32) # rad
 
 def main() -> None:
-    u_0 = jnp.array([1, 2, 3])
-    x_0 = jnp.array([0, 5, 0])
-    ans = f(x_prev=x_0, u=u_0)
-    print(ans)
-    ans = h(x_prev=x_0)
-    print(ans)
+    q = 0.01
+    r = 0.02
 
+    u_0 = jnp.array([1, 2, 3], dtype=jnp.float32)
+    x_0 = jnp.array([0, 5, 0], dtype=jnp.float32)
+    z = jnp.array([0], dtype=jnp.float32)
+    dt = 0.5
+
+    nx = x_0.shape[0]
+    nz = z.shape[0]
+    Q = q * jnp.eye(nz, dtype=jnp.float32)
+    R = r * jnp.eye(nx, dtype=jnp.float32)
+    
+     # assume perfect information initially
+    Sigma_0 = jnp.zeros_like(R, dtype=jnp.float32)
+    mu_0 = x_0
+
+    # call filter
+    my_ekf = EKF(f_sys=f, h_sys=h, Q=Q, R=R)
+    mu, Sigma = my_ekf.propagate(
+        mu_prev=mu_0,
+        Sigma_prev=Sigma_0,
+        u=u_0,
+        z=z,
+        dt=dt
+    )
+    print(f"New mean and covariance are '{mu}', \n'{Sigma}'")
 
 if __name__ == "__main__":
     main()
